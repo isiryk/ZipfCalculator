6db583e4dea69b9a54b262ca0040a01f96e86f42
isiryk <siryk.ivan@gmail.com>
2017-12-23 12:32:37 -0500

    safety if user forgets to input argument

--- ZipfCalc.cpp
+++ ZipfCalc.cpp
@@ -2,7 +2,12 @@
 #include <string>
 
 int main(int argc, char* argv[]){
-  std::string filename = argv[1];
-  std::cout << "This is the filename: " << filename << "\n";
-  return 0;
+  if(argc > 1){
+    std::string filename = argv[1];
+    std::cout << "This is the filename: " << filename << "\n";
+    return 0;
+  } else {
+    std::cout << "Please include filename as an argument" << "\n";
+    return 0;
+  }
 }
