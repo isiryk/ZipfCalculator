d41777002a65c4f67e0eb0774a133164e5d5c7c5
isiryk <siryk.ivan@gmail.com>
2017-12-23 17:24:45 -0500

    We are done, provided is the sample text file and the corresponding output :)

--- ZipfCalc.cpp
+++ ZipfCalc.cpp
@@ -8,8 +8,8 @@
   struct data {
     std::string readWord;
     int countWord;
-    // data(std::string n, int c) : readWord(n), countWord(c) {}
-  };
+    bool operator() (data i, data j) {return (i.countWord>j.countWord);}
+  } data2;
 
   std::vector<data> vec;
 
@@ -41,15 +41,17 @@
     inFile.open(filename.c_str());
     if(!inFile){
       std::cout << "Please enter a valid filename" << "\n";
+      inFile.close();
     } else {
       std::cout << "Success" << "\n";
       std::string word;
       int count = 0;
       while(inFile >> word){
         word.erase(std::remove_if(word.begin(), word.end(), ispunct), word.end());
-        word.toLowerCase();
+        transform(word.begin(), word.end(), word.begin(), ::tolower);
         counter(word);
       }
+      inFile.close();
     }
   }
 
@@ -58,10 +60,14 @@
       std::string filename = argv[1];
       std::cout << "This is the filename: " << filename << "\n";
       readFile(filename);
+      std::sort(vec.begin(), vec.end(), data2);
+      std::ofstream printOut;
+      printOut.open("completed-"+filename);
+      printOut << "Total Words: " << vec.size() << "\n";
       for(int ct = 0; ct < vec.size(); ct++){
-        std::cout << "ReadWord: " << vec[ct].readWord << "\n";
-        std::cout << "Count: " << vec[ct].countWord << "\n";
+        printOut << "------------------------\n" << "Word: " << vec[ct].readWord << "\nCount: " << vec[ct].countWord << "\n" << "------------------------\n";
       }
+      printOut.close();
       return 0;
     } else {
       std::cout << "Please include filename as an argument" << "\n";
